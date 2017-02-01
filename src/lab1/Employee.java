a644c4957a76c79ad2b59255f67a824acbb955ab
isurface <isurface@L11715042778.wctc.msft>
2017-01-31 21:00:06 -0600

    2. Changed properties to private, added setter/getters, validation

--- src/lab1/Employee.java
+++ src/lab1/Employee.java
@@ -12,16 +12,103 @@ import java.util.Date;
  * @version     1.02
  */
 public class Employee {
-    String firstName;
-    String lastName;
-    public String ssn;
-    boolean metWithHr;
-    boolean metDeptStaff;
-    boolean reviewedDeptPolicies;
-    boolean movedIn;
-    String cubeId;
-    Date orientationDate;
+    
+    private String firstName;
 
+    
+    
+    private String lastName;
+    private String ssn;
+   private boolean metWithHr;
+   private boolean metDeptStaff;
+   private boolean reviewedDeptPolicies;
+   private boolean movedIn;
+    private String cubeId;
+    private Date orientationDate;
 
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    public void setFirstName(String firstName){
+       if(firstName == null || firstName.length() < 2){ 
+           System.out.println("error");
+       }
+       
+       
+       
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public boolean isMetWithHr() {
+        return metWithHr;
+    }
+
+    public boolean isMetDeptStaff() {
+        return metDeptStaff;
+    }
+
+    public boolean isReviewedDeptPolicies() {
+        return reviewedDeptPolicies;
+    }
+
+    public boolean isMovedIn() {
+        return movedIn;
+    }
+
+    public String getCubeId() {
+        return cubeId;
+    }
+
+    public Date getOrientationDate() {
+        return orientationDate;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public void setMetWithHr(boolean metWithHr) {
+        this.metWithHr = metWithHr;
+    }
+
+    public void setMetDeptStaff(boolean metDeptStaff) {
+        this.metDeptStaff = metDeptStaff;
+    }
+
+    public void setReviewedDeptPolicies(boolean reviewedDeptPolicies) {
+        this.reviewedDeptPolicies = reviewedDeptPolicies;
+    }
+
+    public void setMovedIn(boolean movedIn) {
+        this.movedIn = movedIn;
+    }
+
+    public void setCubeId(String cubeId) {
+        this.cubeId = cubeId;
+    }
+
+    public void setOrientationDate(Date orientationDate) {
+        this.orientationDate = orientationDate;
+    }
+
+    
+    
+    
 
 }
